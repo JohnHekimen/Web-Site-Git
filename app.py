abb8869c75a6ccc321f0cd2bcf8069af803911fc
JohnHekimen <johnhekemenport@gmail.com>
2026-07-06 08:29:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-import streamlit as st
-import scipy.stats
-import time
 import pandas as pd
+import scipy.stats
+import streamlit as st
+import time
 
 # estas são variáveis persistentes preservadas à medida que o Streamlin executa novamente esse script
 if 'experiment_no' not in st.session_state:
@@ -15,6 +15,7 @@ st.header('Jogando uma moeda')
 chart = st.line_chart([0.5])
 
 def toss_coin(n):
+
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
     mean = None
@@ -22,26 +23,27 @@ def toss_coin(n):
     outcome_1_count = 0
 
     for r in trial_outcomes:
-        outcome_no += 1
+        outcome_no +=1
         if r == 1:
             outcome_1_count += 1
-            mean = outcome_1_count / outcome_no
-            chart.add_rows([mean])
-            time.sleep(0.05)
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
 
     return mean
 
-
-number_of_trials = st.slider('Numero de tentativas?', 1, 1000, 10)
+number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
 start_button = st.button('Executar')
 
 if start_button:
-    st.write(f'Executando o experimento {number_of_trials} tentativas')
-    st.session_state['experimento_no'] += 1
+    st.write(f'Executando o experimento de {number_of_trials} tentativas.')
+    st.session_state['experiment_no'] += 1
     mean = toss_coin(number_of_trials)
     st.session_state['df_experiment_results'] = pd.concat([
         st.session_state['df_experiment_results'],
-        pd.DataFrame(data=[[st.session_state['experimento_no'], number_of_trials, mean]],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]],
                      columns=['no', 'iterations', 'mean'])
         ],
         axis=0)
